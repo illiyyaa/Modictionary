af257cf4a12c8a21659ee4e03011c7f4ecd4dd34
tivanshaker <tidi.ervi@gmail.com>
2020-05-14 19:46:10 +0700

    ISENG AJA
    ISENG AJA

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Modictionary</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,18 +81,20 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Modictionary
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a onclick="tombol1()"  href="#">Cari Modul</a>
+                    <a onclick="tombol1()"  href="#">Cari Asprak</a>
+                    <a onclick="tombol1()"  href="#">Daftar Praktikum</a>
+                    <a href="#">About</a>
                 </div>
+                <script>
+                    function tombol1() {
+                        alert("Login Dulu Ya!");
+                }
+                </script>
             </div>
         </div>
     </body>
