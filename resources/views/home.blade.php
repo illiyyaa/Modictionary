c911c93e0ac323c8d6e77fbcb9c6b52c76fb2fb9
tivanshaker <tidi.ervi@gmail.com>
2020-05-14 20:11:53 +0700

    Update home.blade.php
    add searching box

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Cari Modul Praktikum</div>
+                <div class="card-header">
+                    <form>
+                        <input style="width: 45%" class="search" type="text" placeholder="Cari..." required>
+                        <input class="button" type="button" value="Cari">       
+                    </form>
+
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
