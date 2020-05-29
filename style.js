f018cd045cce5b8cba6de555e32c6fb8fbd6d497
illiyyaa <43401946+illiyyaa@users.noreply.github.com>
2020-05-29 14:41:47 +0700

    style

---
+++ style.js
@@ -0,0 +1,91 @@
+const converted = {
+  h1: { color: "#F24C00", textDecoration: "none", fontWeight: "bold" },
+  h4: {
+    color: "blue",
+    fontStyle: "italic",
+    textDecoration: "underline",
+    fontWeight: "bold"
+  },
+  h5: { fontStyle: "italic", fontWeight: "normal" },
+  h6: { fontStyle: "italic", fontWeight: "normal", fontSize: "90%" },
+  body: {
+    backgroundColor: "#E7E7E7",
+    fontFamily: "sans-serif",
+    overflowY: "scroll",
+    overflowX: "hidden"
+  },
+  ".a": { color: "#e7e7e7", textDecoration: "none" },
+  ".bg": {
+    backgroundColor: "#485696",
+    fontFamily: "sans-serif",
+    width: "150%",
+    height: "70px",
+    fontSize: "24px",
+    paddingLeft: "20px",
+    paddingRight: "10px",
+    marginRight: "20px",
+    marginLeft: "0px"
+  },
+  ".img": { marginTop: "10px", width: "50px" },
+  ".button": {
+    backgroundColor: "#F24C00",
+    border: "none",
+    color: "#e7e7e7",
+    padding: "none",
+    textAlign: "center",
+    textDecoration: "none",
+    fontSize: "16px",
+    margin: "4px 2px",
+    width: "100px",
+    height: "40px",
+    borderRadius: "5%"
+  },
+  ".box": {
+    border: "none",
+    color: "#000000",
+    padding: "none",
+    textAlign: "center",
+    textDecoration: "none",
+    fontSize: "16px",
+    margin: "4px 2px",
+    width: "100px",
+    height: "40px",
+    borderRadius: "5%"
+  },
+  ".boxin": {
+    borderStyle: "none",
+    borderColor: "#00000",
+    color: "#000000",
+    padding: "none",
+    textAlign: "center",
+    textDecoration: "none",
+    fontSize: "16px",
+    margin: "4px 2px",
+    width: "400px",
+    height: "40px",
+    borderRadius: "5%"
+  },
+  "#wrapper": { width: "1500px", margin: "0 auto" },
+  "#sidebar": {
+    background: "#f9c784",
+    width: "190px",
+    cssFloat: "left",
+    position: "fixed",
+    padding: "5px"
+  },
+  "#content": {
+    cssFloat: "left",
+    width: "760px",
+    marginLeft: "300px",
+    padding: "15px",
+    background: "#E7E7E7"
+  },
+  ul: { background: "#485696", padding: "5px" },
+  "ul li": {
+    listStyleType: "none",
+    borderBottom: "1px solid #E7E7E7",
+    marginTop: "20px",
+    marginBottom: "20px"
+  },
+  "ul li a": { textDecoration: "none", color: "#E7E7E7" }
+};
