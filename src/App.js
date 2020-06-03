0a56387597ddfdb63c67ab0015e925d88d1da767
sarah <sarahhasnaz@gmail.com>
2020-06-03 17:54:46 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -1,58 +1,19 @@
 import React from 'react';
-import logo from './logo.png';
+import {BrowserRouter, Route} from 'react-router-dom';
+import home from './halaman/home';
+import logo from './halaman/logo.png';
 import './style.css';
 
 function App() {
   return (
-    <div>
-      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-      <link rel="stylesheet" type="text/css" href="style.css" />
-      <title>Modictionary</title>
-      <header style={{color: '#e7e7e7', backgroundColor: '#485696', display: 'block'}}><img className="img" src={logo} />  Modictionary</header>
-      <div id="wrapper">
-        <div id="sidebar">
-          <ul>
-            <li><a className="a" href="#">Home</a></li>
-            <li><a className="a" href="#">About</a></li>
-            <li><a className="a" href="#">Contact</a></li>
-            <li><a className="a" href="#">Semester 1</a></li>
-            <li><a className="a" href="#">Semester 2</a></li>
-            <li><a className="a" href="#">Semester 3</a></li>
-            <li><a className="a" href="#">Semester 4</a></li>
-            <li><a className="a" href="#">Semester 5</a></li>
-            <li><a className="a" href="#">Semester 6</a></li>
-          </ul>
-        </div>
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={home}></Route>
       </div>
-      {/* 	<i style="color:#3d3d29;">hi</i> */}
-      <div id="content">
-        <center> <h1>Cari Modul Praktikum</h1> 
-          <form action="./search.php" method="get">
-            <input className="boxin" type="text" name="k" size={50} />
-            <br />
-            <b>Sort by</b>
-            <select className="box " name="by">
-              <option value>Sort</option>
-              <option value="ModulID">Id</option>
-              <option value="ModulTitle">Title</option>
-              <option value="MataKuliah">Matkul</option>
-              <option value="Asprak">Asprak</option>
-              <option value="Dosen">Dosen</option>
-              <option value="Semester">Semester</option>
-            </select>
-            <select className="box" name="sort">
-              <option value>None</option>
-              <option value="Asc">Asc</option>
-              <option value="Desc">Desc</option>
-            </select>
-            <input type="submit" className="button" defaultValue="Search" />
-          </form>
-        </center>
-        {/* bikin paragraf sebanyak mungkin sampai halaman bisa discroll kebawah */}
-      </div>
-    </div>
+    </BrowserRouter>
   );
-  }
+}
+export default App;
   /*return (
     <div className="App">
       <header className="App-header">
@@ -74,4 +35,4 @@ function App() {
   //return <h1>Hello World</h1>
 
 
-export default App;
+
