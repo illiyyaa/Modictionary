38810783bdc2ce011db778fe9e5e86ad6ccc85a4
sarah <sarahhasnaz@gmail.com>
2020-06-08 13:59:45 +0700

    url

--- src/halaman/Home.js
+++ src/halaman/Home.js
@@ -14,7 +14,7 @@ export default function Home(){
   });
 //ambil data
   const getData = async () => {
-    const BASE_URL = "https://qrary-fuseki-service.herokuapp.com/modic-semweb/query";
+    const BASE_URL = "http://qrary-fuseki-service.herokuapp.com/modic-semweb/query";
 
     const headers = {
       'Accept': 'application/sparql-results+json,*/*;q=0.9',
