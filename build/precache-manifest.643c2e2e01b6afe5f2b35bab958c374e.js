64415673675442a1f44c574469f26649c40cc9a6
illiyyaa <43401946+illiyyaa@users.noreply.github.com>
2020-06-08 16:09:15 +0700

    build surge

---
+++ build/precache-manifest.643c2e2e01b6afe5f2b35bab958c374e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b0ad917517dcad0174b5a10a969ff9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fa1b4698531cfbfca50",
+    "url": "/static/css/main.52d2c176.chunk.css"
+  },
+  {
+    "revision": "867d2b01ecc199cbe500",
+    "url": "/static/js/2.20c12f01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.20c12f01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa1b4698531cfbfca50",
+    "url": "/static/js/main.993bdf73.chunk.js"
+  },
+  {
+    "revision": "67c46fa5191a7506d6bc",
+    "url": "/static/js/runtime-main.bbba985a.js"
+  },
+  {
+    "revision": "790c184ca56a72f7077de8087a7349f6",
+    "url": "/static/media/logo.790c184c.png"
+  }
+]);
